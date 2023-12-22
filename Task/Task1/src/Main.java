052d46312719c78a61ddbddfd1b3e2008ca359c9
JakupSinani <106808772+JakupSinani@users.noreply.github.com>
2023-12-22 22:09:57 +0100

    22-12-2023

---
+++ Task/Task1/src/Main.java
@@ -0,0 +1,24 @@
+/*
+
+22-12-2023
+
+Write a program that displays: "I think being a programmer is cool".
+
+        Example output:
+        I think being a programmer is cool
+
+        Requirements:
+        •	The program should output text.
+        •	The text should begin with "I think".
+        •	The text should end with "is cool".
+        •	The text should consist of 34 characters, including spaces and punctuation marks.
+        •	The text should consist of one line.
+        •	The displayed text must match the task conditions.
+
+        */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I think being programmer is cool");
+    }
+}
